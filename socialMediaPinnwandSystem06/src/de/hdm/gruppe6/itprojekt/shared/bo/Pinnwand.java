d87e2e93284a6dcdffe9c02985a1ed455a30a566
KrasniqiT <gk025@hdm-stuttgart.de>
2014-05-05 15:25:56 +0200

    Business Objects Klassen und Kommentar Abo- Mapper 05.05

--- socialMediaPinnwandSystem06/src/de/hdm/gruppe6/itprojekt/shared/bo/Pinnwand.java
+++ socialMediaPinnwandSystem06/src/de/hdm/gruppe6/itprojekt/shared/bo/Pinnwand.java
@@ -1,5 +1,6 @@
 package de.hdm.gruppe6.itprojekt.shared.bo;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 
@@ -9,23 +10,31 @@ public class Pinnwand extends BusinessObject {
 	 * @author Özlem Gül, Michael Schelkle, Bharti Kumar
 	 */
 	private static final long serialVersionUID = 1L;
+
+	private User user;
+	private ArrayList<Abonnement> abo = new ArrayList<Abonnement>();
+	private ArrayList<Textbeitrag> textbeitrag= new ArrayList<Textbeitrag>();
+
+	// Konstruktoren
+	public Pinnwand(ArrayList<Abonnement> abo, ArrayList<Textbeitrag> textbeitrag, User user) {
+		this.user= user;
+		this.textbeitrag=textbeitrag;
+		this.abo=abo;
+	}
+
+	public Pinnwand() {
+	}
+
+	// Getter
+	public ArrayList<Abonnement> getUser() {
+		return abo;
+	}
 	
-	private Vector<Abonnement> abo = new Vector<Abonnement>();
-
-//Konstruktor
-public Pinnwand(Vector<Abonnement> abo){
-	this.abo=abo;
-}
-
-public Pinnwand(){
-}
-
-//Getter und Setter
-public Vector<Abonnement> getAbo() {
-	return abo;
-}
-
-public void setAbonnement(Vector<Abonnement> abo) {
-	this.abo = abo;
-}
+	public User getAbo() {
+		return user;
+	}
+	
+	public ArrayList<Textbeitrag> getTextbeitrag() {
+		return textbeitrag;
+	}
 }
